91d3c87f623f3861f903135c2da905219c893ac2
PierreRochard <pierre.rochard@gmail.com>
2014-08-24 15:47:26 -0400

    added multibit support

--- database/csvimport.py
+++ database/csvimport.py
@@ -27,88 +27,144 @@ import uuid
 import api
 
 def csv_import(csvfile):
-    with open(csvfile, 'rt') as csvfile:
-        # Adding the metadata and the binary file to Memoranda
-        memorandum = {}
-        memorandum['entry_space'] = 'Memoranda'
-        memorandum['unique'] = str(uuid.uuid4())
-        memorandum['Date'] = time.strftime("%Y%m%d-%H%M%S")
-        memorandum['Filename'] = csvfile.name
-        memorandum['Filetype'] = 'csv'
-        csvfile.seek(0, os.SEEK_END)
-        memorandum['Filesize'] = csvfile.tell()
-        csvbinary = io.StringIO(csvfile.read())
-        csvbinary = csvbinary.getvalue()
-        memorandum['File'] = csvbinary
-        api.add_record(memorandum)
+  with open(csvfile, 'rt') as csvfile:
+    # Adding the metadata and the binary file to Memoranda
+    memorandum = {}
+    memorandum['entry_space'] = 'Memoranda'
+    memorandum['unique'] = str(uuid.uuid4())
+    memorandum['Date'] = time.strftime("%Y%m%d-%H%M%S")
+    memorandum['Filename'] = csvfile.name
+    memorandum['Filetype'] = 'csv'
+    csvfile.seek(0, os.SEEK_END)
+    memorandum['Filesize'] = csvfile.tell()
+    csvbinary = io.StringIO(csvfile.read())
+    csvbinary = csvbinary.getvalue()
+    memorandum['File'] = csvbinary
+    api.add_record(memorandum)
 
-        # Adding individual CSV lines to the GeneralJournal and GeneralLedger
-        csvfile.seek(0)
-        reader = csv.reader(csvfile)
-        # Numbering each row
-        reader = enumerate(reader)
-        # Create a list of lists:
-        rows = [pair for pair in reader]
-        # Find the first longest list:
-        header = max(rows,key=lambda tup:len(tup[1]))
-        # print(header)
-        is_BitcoinCore = header[1] == ['Confirmed', 'Date', 'Type', 'Label', 'Address', 'Amount', 'ID']
-        is_MultiBit = header[1] == ['Date', 'Description', 'Amount (BTC)', 'Amount ($)', 'Transaction Id']
+    # Adding individual CSV lines to the GeneralJournal and GeneralLedger
+    csvfile.seek(0)
+    reader = csv.reader(csvfile)
+    # Numbering each row
+    reader = enumerate(reader)
+    # Create a list of lists:
+    rows = [pair for pair in reader]
+    # Find the first longest list:
+    header = max(rows,key=lambda tup:len(tup[1]))
+    # print(header)
+    is_BitcoinCore = header[1] == ['Confirmed', 'Date', 'Type', 'Label', 'Address', 'Amount', 'ID']
+    is_MultiBit = header[1] == ['Date', 'Description', 'Amount (BTC)', 'Amount ($)', 'Transaction Id']
 
-        # Create a list that contains all the rows under the header:
-        tx_list = []
-        for row in rows:
-            is_transaction = row[0] > header[0]
-            if is_transaction:
-                transaction = row[1]
-                tx_list.append(transaction)
+    if is_BitcoinCore:
+      # Create a list that contains all the rows under the header:
+      tx_list = []
+      for row in rows:
+        is_transaction = row[0] > header[0]
+        if is_transaction:
+          transaction = row[1]
+          tx_list.append(transaction)
 
-        if is_BitcoinCore:
-          for tx in tx_list:
-              record = zip(header[1], tx)
-              record = dict(record)
-              journal_entry = {}
-              journal_entry['entry_space'] = 'GeneralJournal'
-              journal_entry['unique'] = str(uuid.uuid4())
-              journal_entry['Date'] = record['Date']
-              journal_entry['Debits'] = []
-              journal_entry['Credits'] = []
+      for tx in tx_list:
+        record = zip(header[1], tx)
+        record = dict(record)
 
-              debit_ledger_entry = {}
-              debit_ledger_entry['entry_space'] = 'GeneralLedger'
-              debit_ledger_entry['unique'] = str(uuid.uuid4())
-              journal_entry['Debits'].append(debit_ledger_entry['unique'])
-              debit_ledger_entry['Date'] = record['Date']
-              debit_ledger_entry['Type'] = "Debit"
-              debit_ledger_entry['Amount'] = int(abs(float(record['Amount']))*100000000)
-              debit_ledger_entry['Unit'] = "satoshis"
-              debit_ledger_entry['gjUnique'] = journal_entry['unique']
+        journal_entry = {}
+        journal_entry['entry_space'] = 'GeneralJournal'
+        journal_entry['unique'] = str(uuid.uuid4())
+        journal_entry['Date'] = record['Date']
+        journal_entry['Debits'] = []
+        journal_entry['Credits'] = []
 
-              credit_ledger_entry = {}
-              credit_ledger_entry['entry_space'] = 'GeneralLedger'
-              credit_ledger_entry['unique'] = str(uuid.uuid4())
-              journal_entry['Credits'].append(debit_ledger_entry['unique'])
-              journal_entry['Credits']
-              credit_ledger_entry['Date'] = record['Date']
-              credit_ledger_entry['Type'] = "Credit"
-              credit_ledger_entry['Amount'] = int(abs(float(record['Amount']))*100000000)
-              credit_ledger_entry['Unit'] = "satoshis"
-              credit_ledger_entry['gjUnique'] = journal_entry['unique']
+        debit_ledger_entry = {}
+        debit_ledger_entry['entry_space'] = 'GeneralLedger'
+        debit_ledger_entry['unique'] = str(uuid.uuid4())
+        journal_entry['Debits'].append(debit_ledger_entry['unique'])
+        debit_ledger_entry['Date'] = record['Date']
+        debit_ledger_entry['Type'] = "Debit"
+        debit_ledger_entry['Amount'] = int(abs(float(record['Amount']))*100000000)
+        debit_ledger_entry['Unit'] = "satoshis"
+        debit_ledger_entry['gjUnique'] = journal_entry['unique']
 
-              if int(abs(float(record['Amount']))*100000000) > 0:
-                debit_ledger_entry['Account'] = "Bitcoins"
-                credit_ledger_entry['Account'] = "Revenue"
-              elif int(abs(float(record['Amount']))*100000000) < 0:
-                debit_ledger_entry['Account'] = "Expense"
-                credit_ledger_entry['Account'] = "Bitcoins"
-              journal_entry['Debits']= set(journal_entry['Debits'])
-              journal_entry['Credits']= set(journal_entry['Credits'])
-              api.add_record(journal_entry)
-              api.add_record(debit_ledger_entry)
-              api.add_record(credit_ledger_entry)
-        elif is_MultiBit:
-          pass
-    return True
+        credit_ledger_entry = {}
+        credit_ledger_entry['entry_space'] = 'GeneralLedger'
+        credit_ledger_entry['unique'] = str(uuid.uuid4())
+        journal_entry['Credits'].append(debit_ledger_entry['unique'])
+        journal_entry['Credits']
+        credit_ledger_entry['Date'] = record['Date']
+        credit_ledger_entry['Type'] = "Credit"
+        credit_ledger_entry['Amount'] = int(abs(float(record['Amount']))*100000000)
+        credit_ledger_entry['Unit'] = "satoshis"
+        credit_ledger_entry['gjUnique'] = journal_entry['unique']
+
+        if int(abs(float(record['Amount']))*100000000) > 0:
+          debit_ledger_entry['Account'] = "Bitcoins"
+          credit_ledger_entry['Account'] = "Revenue"
+        elif int(abs(float(record['Amount']))*100000000) < 0:
+          debit_ledger_entry['Account'] = "Expense"
+          credit_ledger_entry['Account'] = "Bitcoins"
+        journal_entry['Debits']= set(journal_entry['Debits'])
+        journal_entry['Credits']= set(journal_entry['Credits'])
+        api.add_record(journal_entry)
+        api.add_record(debit_ledger_entry)
+        api.add_record(credit_ledger_entry)
+
+    elif is_MultiBit:
+      # Create a list that contains all the rows under the header:
+      tx_list = []
+      for row in rows:
+        is_transaction = row[0] > header[0]
+        if is_transaction:
+          transaction = row[1]
+          tx_list.append(transaction)
+
+      for tx in tx_list:
+        record = zip(header[1], tx)
+        record = dict(record)
+      record = zip(header[1], tx)
+      record = dict(record)
+
+      journal_entry = {}
+      journal_entry['entry_space'] = 'GeneralJournal'
+      journal_entry['unique'] = str(uuid.uuid4())
+      journal_entry['Date'] = record['Date']
+      journal_entry['Debits'] = []
+      journal_entry['Credits'] = []
+
+      debit_ledger_entry = {}
+      debit_ledger_entry['entry_space'] = 'GeneralLedger'
+      debit_ledger_entry['unique'] = str(uuid.uuid4())
+      journal_entry['Debits'].append(debit_ledger_entry['unique'])
+      debit_ledger_entry['Date'] = record['Date']
+      debit_ledger_entry['Type'] = "Debit"
+      debit_ledger_entry['Amount'] = int(abs(float(record['Amount (BTC)']))*100000000)
+      debit_ledger_entry['Unit'] = "satoshis"
+      debit_ledger_entry['gjUnique'] = journal_entry['unique']
+
+      credit_ledger_entry = {}
+      credit_ledger_entry['entry_space'] = 'GeneralLedger'
+      credit_ledger_entry['unique'] = str(uuid.uuid4())
+      journal_entry['Credits'].append(debit_ledger_entry['unique'])
+      journal_entry['Credits']
+      credit_ledger_entry['Date'] = record['Date']
+      credit_ledger_entry['Type'] = "Credit"
+      credit_ledger_entry['Amount'] = int(abs(float(record['Amount (BTC)']))*100000000)
+      credit_ledger_entry['Unit'] = "satoshis"
+      credit_ledger_entry['gjUnique'] = journal_entry['unique']
+
+      if int(abs(float(record['Amount (BTC)']))*100000000) > 0:
+        debit_ledger_entry['Account'] = "Bitcoins"
+        credit_ledger_entry['Account'] = "Revenue"
+      elif int(abs(float(record['Amount (BTC)']))*100000000) < 0:
+        debit_ledger_entry['Account'] = "Expense"
+        credit_ledger_entry['Account'] = "Bitcoins"
+      journal_entry['Debits']= set(journal_entry['Debits'])
+      journal_entry['Credits']= set(journal_entry['Credits'])
+      api.add_record(journal_entry)
+      api.add_record(debit_ledger_entry)
+      api.add_record(credit_ledger_entry)
+    else:
+      print("Unrecognized file format")
+  return True
 
 def main():
     if len(sys.argv) <2:
