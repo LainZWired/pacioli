557acd47e90a6014baf4968e5d0f78219bfef9bc
Pierre Rochard <pierre@bitpay.com>
2014-10-13 23:04:31 -0400

    removed mappings structure

--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ def upload():
                 file.seek(0, os.SEEK_END)
                 fileSize = file.tell()
                 fileText = file.stream.getvalue()
-                memo = models.Memoranda(id=id, date=uploadDate, fileName=fileName, fileType=fileType, file=fileText, fileSize=fileSize, fileMap_id = "")
+                memo = models.Memoranda(id=id, date=uploadDate, fileName=fileName, fileType=fileType, file=fileText, fileSize=fileSize)
                 db.session.add(memo)
                 db.session.commit()
                 filenames.append(fileName)
@@ -41,55 +41,3 @@ def upload():
         title = 'Upload',
         filenames=filenames,
         memos=memos)
-
-
-@app.route('/FileMaps', methods=['POST','GET'])
-def filemaps():
-    form = forms.NewFileMap()
-    if request.method == 'POST':
-      id = str(uuid.uuid4())
-      fileMapName = form.fileMapName.data
-      fileMap = models.FileMaps(id=id, fileMapName=fileMapName)
-      db.session.add(fileMap)
-      db.session.commit()
-    fileMaps = models.FileMaps.query.order_by(models.FileMaps.fileMapName.desc()).all()
-    return render_template('filemaps.html', 
-      fileMaps=fileMaps,
-      form=form)
-
-@app.route('/FileMaps/delete/<fileMapName>')
-def deletefilemap(fileMapName):
-    fileMap = models.FileMaps.query.filter_by(fileMapName=fileMapName).first()
-    db.session.delete(fileMap)
-    db.session.commit()
-    fileMaps = models.FileMaps.query.order_by(models.FileMaps.fileMapName.desc()).all()
-    form = forms.NewFileMapping()
-    return redirect(url_for('filemaps'))
-
-
-@app.route('/FileMaps/<fileMapName>', methods=['POST','GET'])
-def filemap(fileMapName):
-    form = forms.NewFileMapping()
-    fileMap = models.FileMaps.query.filter_by(fileMapName=fileMapName).first()
-    fileMapID = fileMap.id
-    fileMappings = models.FileMappings.query.filter_by(fileMap_id=fileMapID).all()
-    if request.method == 'POST':
-      id = str(uuid.uuid4())
-      fileMappingName = form.fileMappingName.data
-      fileMappingType = form.fileMappingType.data
-      fileMappingArgument = form.fileMappingArgument.data
-      fileMappingValue = form.fileMappingValue.data
-      fileMappingEntry = models.FileMappings(id=id, fileMap_id=fileMapID, fileMappingName=fileMappingName, fileMapping={"idtype":fileMappingType,"argument":fileMappingArgument, "value":fileMappingValue})
-      db.session.add(fileMappingEntry)
-      db.session.commit()
-      return redirect(url_for('filemap', fileMapName = fileMap.fileMapName))
-    return render_template('filemap.html', fileMappings=fileMappings, fileMapName=fileMapName, form=form)
-
-@app.route('/FileMaps/delete/<fileMapName>/<fileMappingName>')
-def deletefilemapping(fileMapName, fileMappingName):
-  fileMap = models.FileMaps.query.filter_by(fileMapName=fileMapName).first()
-  fileMapID = fileMap.id
-  fileMapping = models.FileMappings.query.filter_by(fileMappingName=fileMappingName, fileMap_id=fileMapID).first()
-  db.session.delete(fileMapping)
-  db.session.commit()
-  return redirect(url_for('filemap', fileMapName = fileMap.fileMapName))
