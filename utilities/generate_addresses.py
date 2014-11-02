99efd0c0c28dd5339be77e9748c67b0e1c456543
Pierre Rochard <pierre@bitpay.com>
2014-11-02 10:16:35 -0500

    cleaning up

--- utilities/generate_addresses.py
+++ utilities/generate_addresses.py
@@ -3,16 +3,17 @@ import csv
 
 access = AuthServiceProxy("http://rpcusername:rpcpassword@127.0.0.1:8332")
 
-number = 100
+number = 50git
+csvfile = open('addresses.csv','a')
+writer = csv.writer(csvfile)
 
 while number != 0:
     try:
-        newaddress = access.getnewaddress('account')
-        csvfile = open('addresses.csv','a')
-        writer = csv.writer(csvfile)
+        newaddress = access.getnewaddress()
         writer.writerow([newaddress])
-        csvfile.close()
-        print number
+        print(number)
         number -= 1
     except:
         pass
+
+csvfile.close()
